35756866ad49b9ef92f7ac076d2348b722ea66c5
Kenzo Banaag <kenzobanaag@ymail.com>
2021-02-18 23:38:18 -0800

    ADD : Added implementation for writing the statistics log file

--- src/main/java/com/kenzo/csvparser/statistics/StatisticsWriter.java
+++ src/main/java/com/kenzo/csvparser/statistics/StatisticsWriter.java
@@ -1,4 +1,25 @@
 package com.kenzo.csvparser.statistics;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class StatisticsWriter {
+
+    private final String[] statLabels = new String[] {"Records Received", "Records Successful", "Records Failed"};
+
+    public void writeStatistics(String fileName, int[] stats) {
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter("output/"+fileName+".log"));
+
+            for(int i = 0; i < stats.length; i++) {
+                writer.append(String.valueOf(stats[i])).append(" ").append(statLabels[i]);
+                writer.newLine();
+            }
+
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }

07f98e53f4487a0ea5360449718d3227b0554bc1
Kenzo Banaag <kenzobanaag@ymail.com>
2021-02-18 23:37:27 -0800

    REFACTOR : Refactored parser to get file statistics

--- src/main/java/com/kenzo/csvparser/fileparser/CSVParser.java
+++ src/main/java/com/kenzo/csvparser/fileparser/CSVParser.java
@@ -2,7 +2,6 @@ package com.kenzo.csvparser.fileparser;
 
 import java.io.*;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class CSVParser implements FileParser{
@@ -17,6 +16,7 @@ public class CSVParser implements FileParser{
     public CSVParser(String filePath) {
         this.filePath = filePath;
         fileName = new File(filePath).getName();
+        fileName = fileName.substring(0, fileName.length()-4);
     }
 
     public boolean isValidFileType() {
@@ -26,12 +26,11 @@ public class CSVParser implements FileParser{
 
     public List<String[]> readThenParse() {
         List<String[]> data = new ArrayList<>();
-
+        // note: Check out file channel for better writing performance
         try {
             BufferedReader reader = new BufferedReader(new FileReader(filePath));
-            BufferedWriter writer = new BufferedWriter(new FileWriter(outfilePath + fileName.substring(0,
-                    fileName.length()-4) + "-bad.csv"));
-            String line;
+            BufferedWriter writer = new BufferedWriter(new FileWriter(outfilePath + fileName + "-bad.csv"));
+            String line; reader.readLine(); // ignore first line
             while ((line = reader.readLine()) != null) {
                 String[] values = line.split(",");
                 if(values.length != VALID_COLUMNS) {
--- src/main/java/com/kenzo/csvparser/fileparser/FileParser.java
+++ src/main/java/com/kenzo/csvparser/fileparser/FileParser.java
@@ -5,4 +5,7 @@ import java.util.List;
 public interface FileParser {
     boolean isValidFileType();
     List<String[]> readThenParse();
+    int getValidCount();
+    int getInvalidCount();
+    String getFileName();
 }

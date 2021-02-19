5d66a471101a435041998138ad28ec7e98ea29a8
Kenzo Banaag <kenzobanaag@ymail.com>
2021-02-18 23:39:16 -0800

    FINISH : Finished coding challenge, implemented support for db access and stat log

--- src/main/java/com/kenzo/csvparser/ParserMain.java
+++ src/main/java/com/kenzo/csvparser/ParserMain.java
@@ -1,20 +1,28 @@
 package com.kenzo.csvparser;
 
+import com.kenzo.csvparser.database.IDatabase;
+import com.kenzo.csvparser.database.SQLiteDatabase;
 import com.kenzo.csvparser.fileparser.CSVParser;
 import com.kenzo.csvparser.fileparser.FileParser;
+import com.kenzo.csvparser.statistics.StatisticsWriter;
 
 import java.util.List;
 import java.util.Scanner;
 
 /*
-* TODO : Validate file being read
-*        Read by: args, or scanner
 *
 * TODO : Connect to SQLite DB
 *           Connection String
+*           Create singleton connection that accepts a database type
+*
+*        We know that the only thing that differs between different SQL connections like mysql, javadb, sqlite is the
+*        connection string. We want to set the connection string/url when the data is valid and is ready to be parsed.
+*        Since we want a single instance of thing connection string/object, we can use the singleton pattern to
+*        establish a single instance of this class, and globally call it when we need the connection string. This
+*        will allow us to easily switch databases if needed to.
 *
 * NOTE : App should be re-runnable
-*           Might need to create a new db every time we run the program, since we will have duplicate data
+*           Need to create a new db every time we run the program, since we will have duplicate data
 *
 * APPROACH REASONING:
 *        Since we have no description of the type of data we're receiving, we will assume that everything is a String
@@ -43,20 +51,24 @@ public class ParserMain {
             System.out.println("File is not a valid csv");
             return;
         }
-
         List<String[]> data = parser.readThenParse();
+        System.out.println("Parsing Successful!");
+
         // [PHASE TWO]
         // create db
+        IDatabase db = new SQLiteDatabase(parser.getFileName());
 
-        // connect? - SQLDatabaseClass
-
-        // create table if not exist - SQLDatabaseClass
+        // create table if not exist
+        db.createTable("dataTable");
 
         // insert good data to db, bad to csv - DatabaseDAO
-        // CRUD - Create, Read, Update, Delete
+        System.out.println(db.insert(data) == 1 ? "Insert Successful" : "Failed Insert");
 
         // create statistics log file
+        StatisticsWriter writer = new StatisticsWriter();
+        writer.writeStatistics(parser.getFileName(), new int[] {parser.getValidCount() + parser.getInvalidCount(),
+                parser.getValidCount(), parser.getInvalidCount()});
 
-        System.out.println("Parsing Successful!");
+        System.out.println("Statistics Log Created");
     }
 }

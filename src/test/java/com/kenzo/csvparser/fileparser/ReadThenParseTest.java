5e126204f3e325d316307316c8eba61537a9a0be
Kenzo Banaag <kenzobanaag@ymail.com>
2021-02-18 18:43:12 -0800

    ADD : Added tests for CSV Parser methods

--- src/test/java/com/kenzo/csvparser/fileparser/ReadThenParseTest.java
+++ src/test/java/com/kenzo/csvparser/fileparser/ReadThenParseTest.java
@@ -1,4 +1,24 @@
 package com.kenzo.csvparser.fileparser;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class ReadThenParseTest {
+
+    FileParser parser;
+
+    @Test
+    public void readValidFileTest_testData() {
+        parser = new CSVParser("input/testData.csv");
+
+        assertEquals(2, parser.readThenParse().size()); // length of valid data
+    }
+
+    @Test
+    public void readValidFileTest_ms3Data() {
+        parser = new CSVParser("input/ms3Interview.csv");
+
+        assertEquals(493, parser.readThenParse().size()); // length of valid data
+    }
 }
--- src/test/java/com/kenzo/csvparser/fileparser/ValidateFileTypeTest.java
+++ src/test/java/com/kenzo/csvparser/fileparser/ValidateFileTypeTest.java
@@ -1,4 +1,48 @@
 package com.kenzo.csvparser.fileparser;
 
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
 public class ValidateFileTypeTest {
+
+    private FileParser parser;
+
+    @Test
+    public void validate_csv() {
+        parser = new CSVParser("test.csv");
+
+        assertTrue(parser.isValidFileType());
+    }
+
+    @Test
+    public void validate_csv_ignoreCase() {
+        parser = new CSVParser("test.CSV");
+
+        assertTrue(parser.isValidFileType());
+    }
+
+    @Test
+    public void validate_txt() {
+        parser = new CSVParser("test.txt");
+
+        assertFalse(parser.isValidFileType());
+    }
+
+    @Test
+    public void validate_txt_ignoreCase() {
+        parser = new CSVParser("test.TXT");
+
+        assertFalse(parser.isValidFileType());
+    }
+
+    @Test
+    public void validate_other() {
+        String[] extensions = new String[] {"pdf", "png", "jpg", "doc", "docx", "rtf"};
+        for(String ext : extensions) {
+            parser = new CSVParser("test."+ext);
+
+            assertFalse(parser.isValidFileType());
+        }
+    }
 }

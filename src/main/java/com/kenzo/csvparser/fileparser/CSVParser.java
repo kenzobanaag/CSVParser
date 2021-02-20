005dc69accc23eae57d71a811da858284e6b2d34
Kenzo Banaag <kenzobanaag@ymail.com>
2021-02-19 19:56:08 -0800

    REFACTOR : Replaced filestream with input and output stream to allow file path inputs from running the jar

--- src/main/java/com/kenzo/csvparser/fileparser/CSVParser.java
+++ src/main/java/com/kenzo/csvparser/fileparser/CSVParser.java
@@ -4,65 +4,78 @@ import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
+/*
+* Description: This class parses all the valid data and converts it to a list of string[]. All the bad data
+*              are written into a file-bad.csv file.
+* */
 public class CSVParser implements FileParser{
 
-    private String filePath;
-    private String fileName;
-    private final String outfilePath = "output/";
     private final int VALID_COLUMNS = 10;
+    private final String filePath;
+    private String fileName;
     private int validCount = 0;
     private int invalidCount = 0;
 
     public CSVParser(String filePath) {
         this.filePath = filePath;
-        fileName = new File(filePath).getName();
-        fileName = fileName.substring(0, fileName.length()-4);
+        fileName = new File(filePath).getName(); // get only the filename
+        fileName = fileName.substring(0, fileName.length()-4); // remove file extension
     }
 
+    /*
+    * Description: Validate if the given file path is a csv extension.
+    * */
     public boolean isValidFileType() {
         // validate file extension
         return (filePath != null && filePath.length() > 4 && filePath.toLowerCase().endsWith(".csv"));
     }
 
+    /*
+    * Description: Reads each line of the csv file. Splits each line by commas, if the string array doesn't contain
+    *              10 elements, it is considered as bad data and will be written to a bad data csv file. If it does, the
+    *              array will be added to the data list and will be returned to the caller.
+    * */
     public List<String[]> readThenParse() {
         List<String[]> data = new ArrayList<>();
-        // note: Check out file channel for better writing performance
         try {
-            BufferedReader reader = new BufferedReader(new FileReader(filePath));
-            BufferedWriter writer = new BufferedWriter(new FileWriter(outfilePath + fileName + "-bad.csv"));
+            BufferedReader reader = new BufferedReader(new InputStreamReader(
+                    new FileInputStream(filePath)));
+            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(
+                    new FileOutputStream("output/" + fileName + "-bad.csv")));
             String line; reader.readLine(); // ignore first line
             while ((line = reader.readLine()) != null) {
                 String[] values = line.split(",");
-                if(values.length != VALID_COLUMNS) {
-                    // append write to a bad file
+                if (values.length != VALID_COLUMNS) {
+                    // append line to a bad file
                     writer.append(line);
                     writer.newLine();
                     ++invalidCount;
-                }
-                else {
+
+                } else {
                     ++validCount;
-                    data.add(values);
+                    data.add(values); // add valid data to list
                 }
             }
-
             reader.close();
             writer.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
-
         return data;
     }
 
-    public int getValidCount() {
-        return validCount;
-    }
+    /*
+    * Description: Returns the count of valid lines
+    * */
+    public int getValidCount() { return validCount; }
 
-    public int getInvalidCount() {
-        return invalidCount;
-    }
+    /*
+     * Description: Returns the count of invalid lines
+     * */
+    public int getInvalidCount() { return invalidCount; }
 
-    public String getFileName() {
-        return fileName;
-    }
+    /*
+     * Description: Returns the filename of the input file
+     * */
+    public String getFileName() { return fileName; }
 }

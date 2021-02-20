20e440bc3fa046f6f9b83a347f1ffa69b1844353
Kenzo Banaag <kenzobanaag@ymail.com>
2021-02-19 19:52:50 -0800

    REFACTOR : Optimized inserts to use a transaction to exponentially decrease insert times

--- src/main/java/com/kenzo/csvparser/database/DataDao.java
+++ src/main/java/com/kenzo/csvparser/database/DataDao.java
@@ -6,56 +6,100 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.List;
 
+/*
+* Description: Concrete implementation of Dao. Allows the insertion of 2 types of data: String[] and List<String>[].
+*              This class takes in the connection string and table name to make the appropriate SQL queries.
+* */
 public class DataDao implements Dao{
 
     private final String url;
-    private final String tableName;
+    private final String insertQuery;
 
+    // only allow the database to have access to dao creation.
     protected DataDao(String connectionUrl, String tableName) {
         url = connectionUrl;
-        this.tableName = tableName;
+        // init insert sql query
+        insertQuery = "INSERT INTO " + tableName + "(a,b,c,d,e,f,g,h,i,j)" +
+                " VALUES(?,?,?,?,?,?,?,?,?,?)";
     }
 
+    /*
+    * Description: Insert method for a single line of data
+    * */
     @Override
     public int insert(String[] data) {
         int res;
-        Connection conn;
-        String sql = "INSERT INTO " + tableName + "(a,b,c,d,e,f,g,h,i,j)" +
-                " VALUES(?,?,?,?,?,?,?,?,?,?)";
+        Connection conn = null;
 
         try {
             conn = DriverManager.getConnection(url);
-            PreparedStatement preparedStatement = conn.prepareStatement(sql);
+            PreparedStatement preparedStatement = conn.prepareStatement(insertQuery);
             for(int i = 0; i < data.length; i++)
-                preparedStatement.setString(i+1, data[i]);
-            res = preparedStatement.executeUpdate();
+                preparedStatement.setString(i+1, data[i]); // build each parameter from A-J
+            res = preparedStatement.executeUpdate(); // insert the built query then finish
         }
         catch (SQLException throwable) {
             res = -1;
             throwable.printStackTrace();
+        }finally {
+            try {
+                if(conn != null) // close connection
+                    conn.close();
+            }
+            catch (SQLException throwable) {
+                throwable.printStackTrace();
+            }
         }
 
         return res;
     }
 
+    /*
+    * Description: Insert method for lists of data lines.
+    * */
     @Override
     public int insert(List<String[]> data) {
         int res;
-        Connection conn;
-        String sql = "INSERT INTO " + tableName + "(a,b,c,d,e,f,g,h,i,j)" +
-                " VALUES(?,?,?,?,?,?,?,?,?,?)";
-
+        Connection conn = null;
+        PreparedStatement preparedStatement= null;
         try {
-            conn = DriverManager.getConnection(url);
-            PreparedStatement preparedStatement = conn.prepareStatement(sql);
-            for(int j = 0; j < data.size(); j++)
-            for(int i = 0; i < data.get(j).length; i++)
-                preparedStatement.setString(i+1, data.get(j)[i]);
-            res = preparedStatement.executeUpdate();
+            res = -1;
+            conn = DriverManager.getConnection(url); // get connection
+            conn.setAutoCommit(false); // allows transaction
+            preparedStatement = conn.prepareStatement(insertQuery);
+            for (String[] datum : data) {
+                for (int i = 0; i < datum.length; i++) {
+                    preparedStatement.setString(i + 1, datum[i]); // build each parameter from A-J
+                }
+                res = preparedStatement.executeUpdate(); // add this execution to connection sql query
+            }
+
+            if(res != 1) // Transactions are all or nothing, if one fails, cancel the whole transaction
+                conn.rollback();
+
+            conn.commit(); // run all generated queries in one go(transaction)
         }
         catch (SQLException throwable) {
-            res = -1;
+            try {
+                if(conn != null) { // ran into an exception while inserting data
+                    conn.rollback(); // so cancel the whole query
+                }
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+            res = -1; // return -1, because transaction failed
             throwable.printStackTrace();
+        }
+        finally {
+            try {
+                if(preparedStatement != null) // close preparedStatement
+                    preparedStatement.close();
+                if(conn != null) // close connection
+                    conn.close();
+            }
+            catch (SQLException throwable) {
+                throwable.printStackTrace();
+            }
         }
 
         return res;
